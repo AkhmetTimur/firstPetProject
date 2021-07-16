32cf37038754084d34bf8c8bfcfdcc00a83e3f5c
Akhmet Timur <massomi@mail.ru>
2021-07-16 17:06:10 +0500

    make admin and delete user

--- fp/admin/routes.py
+++ fp/admin/routes.py
@@ -26,7 +26,7 @@ def admin_panel():
         users = User.query.all()
         return render_template('admin.html', users = users)
 
-@admin.route('/admin/<int:user_id>')
+@admin.route('/admin/delete/<int:user_id>')
 @login_required
 @administrator
 def delete_user(user_id):
@@ -37,8 +37,18 @@ def delete_user(user_id):
     db.session.commit()
     return redirect(url_for('admin.admin_panel'))
 
+@admin.route('/admin/makeadmin/<int:user_id>')
+@login_required
+@administrator
+def makeadmin_user(user_id):
+    user = User.query.filter_by(id=int(user_id)).first()
+    if user.username == "admin":
+        abort(404)
+    flag = not(user.is_admin)
+    user.is_admin = flag
+    db.session.commit()
+    return redirect(url_for('admin.admin_panel'))
 
-        
 
 
 
